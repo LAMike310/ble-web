39d84320b61418eeeb47bf87590e1a33c87753f3
Mikey Kones <mvg210@gmail.com>
2023-03-26 13:33:44 -0500

    Send new data after 5 seconds

--- components/BluetoothConnection.js
+++ components/BluetoothConnection.js
@@ -20,43 +20,59 @@ const BluetoothConnection = () => {
   const sendDataToDevice = async () => {
     if (!device) {
       setError("No device connected");
+      requestDevice();
       return;
     }
+    let server;
     try {
-      console.log(device);
-      const server = await device.gatt.connect();
-      console.log(server);
+      // stop writing value to device
+      // Connect to GATT server
+      server = await device.gatt.connect();
+      // stop all GATT operations
+
       const service = await server.getPrimaryService(
         "fbba4179-b71a-4db7-8b48-6ff849aba480"
       );
       const characteristic = await service.getCharacteristic(
         "4f36693f-b7a4-4e29-981f-cb00bb0d38d9"
       );
+
       // random 4 digit number
-      const randomNumber = Math.floor(1000 + Math.random() * 9000);
-      // convert to string
-      const randomNumberString = randomNumber.toString();
-      // only length of 4
-      const randomNumberString4 = randomNumberString.substring(0, 4);
-      const data = new TextEncoder().encode(randomNumberString4);
+      const createRandomNumber = () =>
+        Math.floor(1000 + Math.random() * 9000)
+          .toString()
+          .substring(0, 4);
+      // create 10 random numbers
+      const arrayOfRandomNumbers = Array.from({ length: 5 }, () =>
+        createRandomNumber()
+      );
+      // convert array to string
+      const stringOfRandomNumbers = arrayOfRandomNumbers.join(",");
+      console.log(stringOfRandomNumbers);
+      // Disconnect after 10 seconds
+      setTimeout(async () => {
+        sendDataToDevice();
+      }, 5000);
+      const data = new TextEncoder().encode(stringOfRandomNumbers);
       await characteristic.writeValue(data);
       console.log("Data sent");
     } catch (err) {
       setError(err.message);
     }
   };
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     sendDataToDevice();
+  //   }, 1000);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      sendDataToDevice();
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [device]);
+  //   return () => clearInterval(intervalId);
+  // }, [device]);
 
   return (
     <div>
       <button onClick={requestDevice}>Connect to device</button>
+      {/* send data button */}
+      <button onClick={sendDataToDevice}>Send data</button>
       {error && <p>Error: {error}</p>}
       {device && <p>Connected to {device.name}</p>}
     </div>
